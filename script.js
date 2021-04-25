d9755cb7e69a3565508aef3e9ddefdb949cca350
Marcus <marcuslt95@gmail.com>
2021-04-25 20:12:12 +0100

    Wrrestling with event listeners

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //Globals
 
 const grid = document.querySelector('#grid');
-const gridSizeSelect = document.querySelector('#grid-size-select');
+const gridSizeSelect = document.querySelector('#select-btn');
+let cells;
+let isDrawing = false;
 
-let gss = gridSizeSelector;
+//let gss = gridSizeSelector;
 
 //On page load
 
-/*for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {
     let gridItem = document.createElement('div');
     gridItem.classList.add('grid-item')
     grid.appendChild(gridItem);
-}*/
+}
 
 for (let i = 16; i < 101; i++) {
     let option = document.createElement('option');
@@ -21,16 +23,39 @@ for (let i = 16; i < 101; i++) {
 
 //Functions
 
-function gridSizeSelector(){
+function gridCellGenerator() {
+    grid.innerHTML = "";
+    let x = gridSizeSelector();
+
+    for (let i = 0; i < x ** 2; i++) {
+    let gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
+    grid.appendChild(gridItem);
+}
+    cells = document.querySelectorAll('.grid-item');
+    eventListeners();
+}
+
+function gridSizeSelector() {
     return(gridSizeSelect.selectedIndex + 16);
 }
 
-function gridCellGenerator(gss) {
-    alert(gss);
-/*    for (let i = 0; i < gss ** 2; i++) {
-        let gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item')
-        grid.appendChild(gridItem);
-}*/
+function eventListeners() {
+    /*cells.forEach(element => {
+        addEventListener('mousedown', () => {
+             isDrawing = true;
+             
+        });
+    });
+    cells.forEach(element => {
+        addEventListener('mouseup', () => {
+             isDrawing = false;
+             
+        });
+    });*/
+    cells.forEach(element => {
+        addEventListener('mouseover', () => {
+            element.style.backgroundColor = 'red';             
+        });
+    });
 }
-
