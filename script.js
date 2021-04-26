d2946ecde440a2627a8b82611837d005e605f753
Marcus <marcuslt95@gmail.com>
2021-04-26 17:39:22 +0100

    Cleaned up code

--- script.js
+++ script.js
@@ -3,18 +3,13 @@
 const grid = document.querySelector('#grid');
 const gridSizeSelect = document.querySelector('#select-btn');
 let isDrawing = false;
-
+let cells;
 
 //let gss = gridSizeSelector;
 
 //On page load
 
-for (let i = 0; i < 256; i++) {
-    let gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.setAttribute('draggable', 'disabled')
-    grid.appendChild(gridItem);
-}
+gridGenerator();
 
 for (let i = 16; i < 101; i++) {
     let option = document.createElement('option');
@@ -22,33 +17,9 @@ for (let i = 16; i < 101; i++) {
     gridSizeSelect.appendChild(option);
 }
 
-let cells = document.querySelectorAll('.grid-item');
-
-cells.forEach(element => {
-    element.addEventListener('mousedown', () => isDrawing = true)
-})
-
-cells.forEach(element => {
-    element.addEventListener('mouseup', () => isDrawing = false)
-})
-
-cells.forEach(element => {
-    element.addEventListener('mouseenter', () => {
-        if (isDrawing === true) {
-            element.style.backgroundColor = 'blue';
-        }
-    })
-})
-
-cells.forEach(element => {
-    element.addEventListener('dragstart', (e) => {
-        e.preventDefault();
-    })
-})
-
 //Functions
 
-function gridCellGenerator() {
+function gridGenerator() {
     let x = gridSizeSelector();
     grid.innerHTML = "";
     grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
@@ -58,11 +29,36 @@ function gridCellGenerator() {
     for (let i = 0; i < x ** 2; i++) {
     let gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
-    grid.appendChild(gridItem);
+    grid.appendChild(gridItem); 
 }
     cells = document.querySelectorAll('.grid-item');
+    listenerGenerator();
 }
 
 function gridSizeSelector() {
     return(gridSizeSelect.selectedIndex + 16);
+}
+
+function listenerGenerator() {
+    cells.forEach(element => {
+        element.addEventListener('mousedown', () => isDrawing = true)
+    })
+    
+    cells.forEach(element => {
+        element.addEventListener('mouseup', () => isDrawing = false)
+    })
+    
+    cells.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            if (isDrawing === true) {
+                element.style.backgroundColor = 'blue';
+            }
+        })
+    })
+    
+    cells.forEach(element => {
+        element.addEventListener('dragstart', (e) => {
+            e.preventDefault();
+        })
+    })   
 }
