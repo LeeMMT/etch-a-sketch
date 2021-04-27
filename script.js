b88d2a151cf9798d14d9c289b016e05a4f963be8
Marcus <marcuslt95@gmail.com>
2021-04-27 17:31:13 +0100

    Confuddled

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const grid = document.querySelector('#grid');
 const gridSizeSelect = document.querySelector('#select-btn');
 let isDrawing = false;
 let cells;
+const warm = ['780116', 'f7b538', 'db7c26', 'd8572a', 'c32f27'];
+const cold = ['133c55', '386fa4', '59a5d8', '84d2f6', '91e5f6'];
+const rainbow = ['f94144', 'f3722c', 'f8961e', 'f9c74f', '90be6d', '43aa8b', '577590'];
+
+//colourStyle function references
+
+const blackStyle = function(element) {
+    if (isDrawing === true) {
+        element.style.backgroundColor = 'black';  
+}
+}
 
 //On page load
 
@@ -17,8 +28,7 @@ for (let i = 16; i < 101; i++) {
 
 //Functions
 
-function gridGenerator() {
-    let x = gridSizeSelector();
+function gridGenerator(x) {
     grid.innerHTML = "";
     grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${x}, 1fr)`;
@@ -34,7 +44,8 @@ function gridGenerator() {
 }
 
 function gridSizeSelector() {
-    return(gridSizeSelect.selectedIndex + 16);
+    let x = gridSizeSelect.selectedIndex + 16;
+    gridGenerator(x);
 }
 
 function listenerGenerator() {
@@ -47,16 +58,18 @@ function listenerGenerator() {
     })
     
     cells.forEach(element => {
-        element.addEventListener('mouseenter', () => {
-            if (isDrawing === true) {
-                element.style.backgroundColor = 'blue';
-            }
-        })
-    })
-    
-    cells.forEach(element => {
         element.addEventListener('dragstart', (e) => {
             e.preventDefault();
         })
-    })   
+    })
+    
+    colorStyle();   
+}
+
+function colorStyle(x) {
+    if (x === undefined || x === 'black') {
+        cells.forEach(element => {
+            element.addEventListener('mouseenter', blackStyle(e, element))
+    })
+    } 
 }
